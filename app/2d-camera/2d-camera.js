1d4fd36cd3bfad8c795da9d79d7e48c80105be83
Gerhard Meyer <gerhard.meyer@pragmaworld.net>
2022-05-27 15:21:38 +0200

    cleanup

--- app/2d-camera/2d-camera.js
+++ app/2d-camera/2d-camera.js
@@ -14,8 +14,7 @@ export default class FaceSelect extends crsbinding.classes.ViewBase {
 
         if (this.canvas.dataset.ready == "true") {
             ready();
-        }
-        else {
+        } else {
             this.canvas.addEventListener("ready", ready);
         }
     }
@@ -24,16 +23,33 @@ export default class FaceSelect extends crsbinding.classes.ViewBase {
         this.canvas.__layers[0].onPointerDown = null;
         this.canvas.__layers[0].pointerMove = null;
         this.canvas.__layers[0].pointerUp = null;
-
+        this.bgPlane.dispose();
+        this.bgPlane = null;
+        this.canvas = null;
     }
 
     async addMeshes() {
         await this.createPlane()
 
         const meshes = [];
-        meshes.push(await crs.call("gfx_geometry", "add", { element:  this.canvas, data: "icons/home", position: {x: -1, y: 1}, color: "#ff0090" }));
-        meshes.push(await crs.call("gfx_geometry", "add", { element:  this.canvas, data: "flowchart/documents", position: {x: 1, y:2}, color: "#ff9000" }));
-        meshes.push(await crs.call("gfx_geometry", "add", { element:  this.canvas, data: "floorplan/fire_hose", position: {x: 0, y: 0, z:-0.1}, color: "#9000ff" }));
+        meshes.push(await crs.call("gfx_geometry", "add", {
+            element: this.canvas,
+            data: "icons/home",
+            position: {x: -1, y: 1},
+            color: "#ff0090"
+        }));
+        meshes.push(await crs.call("gfx_geometry", "add", {
+            element: this.canvas,
+            data: "flowchart/documents",
+            position: {x: 1, y: 2},
+            color: "#ff9000"
+        }));
+        meshes.push(await crs.call("gfx_geometry", "add", {
+            element: this.canvas,
+            data: "floorplan/fire_hose",
+            position: {x: 0, y: 0},
+            color: "#9000ff"
+        }));
 
 
         const gizmoManager = new BABYLON.GizmoManager(this.canvas.__layers[0])
@@ -42,12 +58,14 @@ export default class FaceSelect extends crsbinding.classes.ViewBase {
     }
 
     async createPlane() {
-
-        this.bgPlane = BABYLON.MeshBuilder.CreatePlane("plane", {size: 200, position: {z:-0.0001}}, this.canvas.__layers[0]);
-        this.bgPlane.material =  new BABYLON.GridMaterial("grid", this.canvas.__layers[0]);
+        this.bgPlane = BABYLON.MeshBuilder.CreatePlane("plane", {
+            size: 200,
+            position: {z: -0.0001}
+        }, this.canvas.__layers[0]);
+        this.bgPlane.material = new BABYLON.GridMaterial("grid", this.canvas.__layers[0]);
         this.bgPlane.material.gridRatio = 0.1;
-        this.bgPlane.material.mainColor = new BABYLON.Color3(1,1,1);
-        this.bgPlane.material.lineColor = new BABYLON.Color3(0.6,0.6,0.6);
+        this.bgPlane.material.mainColor = new BABYLON.Color3(1, 1, 1);
+        this.bgPlane.material.lineColor = new BABYLON.Color3(0.6, 0.6, 0.6);
         this.bgPlane.enablePointerMoveEvents = true;
     }
 
@@ -58,37 +76,37 @@ export default class FaceSelect extends crsbinding.classes.ViewBase {
     }
 
     async pointerDown(event, pickResult) {
-        if(event.button === 0 && pickResult.pickedMesh === this.bgPlane) {
+        if (event.button === 0 && pickResult.pickedMesh === this.bgPlane) {
             this.pickedPoint = pickResult.pickedPoint;
-            this.selectionPlane = BABYLON.MeshBuilder.CreatePlane("plane", { size:0}, this.canvas.__layers[0]);
-            this.selectionPlane.scaling.x =0;
-            this.selectionPlane.scaling.y =0;
+            this.selectionPlane = BABYLON.MeshBuilder.CreatePlane("plane", {size: 0}, this.canvas.__layers[0]);
+            this.selectionPlane.scaling.x = 0;
+            this.selectionPlane.scaling.y = 0;
             this.selectionPlane.setPivotPoint(new BABYLON.Vector3(-0.5, -0.5, 0));
 
-            this.selectionPlane.position = this.pickedPoint.add(new BABYLON.Vector3(0.5,0.5,0));
-                this.selectionPlane.material = await crs.call("gfx_materials", "get", {
-                    element: this.canvas,
-                    value: "#56b6ff"
-                });
+            this.selectionPlane.position = this.pickedPoint.add(new BABYLON.Vector3(0.5, 0.5, 0));
+            this.selectionPlane.material = await crs.call("gfx_materials", "get", {
+                element: this.canvas,
+                value: "#56b6ff"
+            });
             this.selectionPlane.material.alpha = 0.5;
             this.selectionPlane.enableEdgesRendering();
             this.selectionPlane.edgesWidth = 2.0;
-            this.selectionPlane.edgesColor = new BABYLON.Color4(0.2, 0.5, 1,1);
+            this.selectionPlane.edgesColor = new BABYLON.Color4(0.2, 0.5, 1, 1);
         }
     }
 
     async pointerMove(event, pickResult) {
-        if(this.selectionPlane) {
+        if (this.selectionPlane) {
             const width = pickResult.pickedPoint.x - this.pickedPoint.x;
             const height = pickResult.pickedPoint.y - this.pickedPoint.y;
 
-           this.selectionPlane.scaling.x =width;
-           this.selectionPlane.scaling.y =height;
+            this.selectionPlane.scaling.x = width;
+            this.selectionPlane.scaling.y = height;
         }
     }
 
     async pointerUp() {
-        if(this.selectionPlane) {
+        if (this.selectionPlane) {
             this.selectionPlane.dispose();
             this.selectionPlane = null;
         }
