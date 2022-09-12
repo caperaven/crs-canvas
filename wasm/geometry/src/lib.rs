74c2f3b6d4035cbc0befde34db1e87fafd8f1d45
caperaven <caperaven@gmail.com>
2022-09-12 22:41:21 +0200

    add wasm for geometry

--- wasm/geometry/src/lib.rs
+++ wasm/geometry/src/lib.rs
@@ -1,6 +1,28 @@
+mod extrude;
+mod utils;
+
 use wasm_bindgen::prelude::*;
+use js_sys::{Object};
+use lyon::path::Path;
+use lyon::math::{point};
+use crate::utils::get_aabb;
+
+// https://docs.rs/lyon/latest/lyon/
 
 #[wasm_bindgen]
-pub fn line_geometry() {
+pub fn init_panic_hook() {
+    console_error_panic_hook::set_once();
+}
 
+#[wasm_bindgen]
+pub fn line_geometry(x1: f32, y1: f32, x2: f32, y2: f32) -> Object {
+    let mut builder = Path::builder();
+    builder.begin(point(x1, y1));
+    builder.line_to(point(x2, y2));
+    builder.close();
+    let path = builder.build();
+    let aabb = get_aabb(&path);
+    let poly_builder = extrude::extrude_path(&path);
+
+    utils::populate_from_buffer(&poly_builder, &aabb)
 }
