44a1668b1e5045a6f9fb85b356f5700f7a1a0b20
Kieran-Reilly <kieranpreilly@gmail.com>
2022-10-05 12:06:54 +0200

    uncommenting test

--- tests/wasm/geometry/geometry.test.js
+++ tests/wasm/geometry/geometry.test.js
@@ -1,15 +1,15 @@
-// import { assertEquals } from "https://deno.land/std@0.155.0/testing/asserts.ts";
-// import init, {init_panic_hook, line_geometry} from "../../../bin/geometry.js";
-//
-// await init();
-// init_panic_hook();
-//
-// Deno.test("line_geometry", () => {
-//     const result = line_geometry(0, 0, 100, 0);
-//     assertEquals(result.vertices.length, 12);
-//     assertEquals(result.indices.length, 6);
-//     assertEquals(result.aabb.minX, 0);
-//     assertEquals(result.aabb.minY, 0);
-//     assertEquals(result.aabb.maxX, 100);
-//     assertEquals(result.aabb.maxY, 0);
-// })
+import { assertEquals } from "https://deno.land/std@0.155.0/testing/asserts.ts";
+import init, {init_panic_hook, line_geometry} from "../../../bin/geometry.js";
+
+await init();
+init_panic_hook();
+
+Deno.test("line_geometry", () => {
+    const result = line_geometry(0, 0, 100, 0);
+    assertEquals(result.vertices.length, 12);
+    assertEquals(result.indices.length, 6);
+    assertEquals(result.aabb.minX, 0);
+    assertEquals(result.aabb.minY, 0);
+    assertEquals(result.aabb.maxX, 100);
+    assertEquals(result.aabb.maxY, 0);
+})
