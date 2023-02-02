969a7f4de4bae9555a47368ef683bf9203d3f088
Gerhard Meyer <gerhard.meyer@pragmaworld.net>
2023-02-02 14:23:11 +0200

    Update canvas_2d.js

--- components/canvas_2d/canvas_2d.js
+++ components/canvas_2d/canvas_2d.js
@@ -8,7 +8,7 @@ export class Canvas2d extends HTMLElement {
         const camera = this.dataset.camera || "free,0,5,-10";
         const color = this.dataset.color;
         await crs.call("gfx", "initialize", { element: this.#canvas, camera: camera, color: color });
-        await crs.call("component", "notify_ready", {element: this});
+        await crs.call("component", "notify_ready", {element: this.#canvas});
     }
 
     async disconnectedCallback() {
